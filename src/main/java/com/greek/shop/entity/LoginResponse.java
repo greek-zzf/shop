318155e76bd161b792001d0121a679b768d3ee82
Zhouzf <aaaa159557190@gmail.com>
2021-11-10 13:53:35 +0800

    fix error chckstyle plugin config path

---
+++ src/main/java/com/greek/shop/entity/LoginResponse.java
@@ -0,0 +1,8 @@
+package com.greek.shop.entity;
+
+/**
+ * @author Zhaofeng Zhou
+ * @date 2021/11/10/010 11:02
+ */
+public class LoginResponse {
+}
---
+++ src/main/java/com/greek/shop/interceptor/UserLoginInterceptor.java
@@ -0,0 +1,8 @@
+package com.greek.shop.interceptor;
+
+/**
+ * @author Zhaofeng Zhou
+ * @date 2021/11/10/010 10:17
+ */
+public class UserLoginInterceptor {
+}
