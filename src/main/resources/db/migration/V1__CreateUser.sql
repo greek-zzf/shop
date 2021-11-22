4dd0283ace7af130b8a9e47b3437dec09d8d871f
Zhaofeng Zhou <aaaa159557190@gmail.com>
2021-11-22 21:49:53 +0800

    add data

--- src/main/resources/db/migration/V1__CreateUser.sql
+++ src/main/resources/db/migration/V1__CreateUser.sql
@@ -1,10 +1,14 @@
 create table USER
 (
-    ID bigint primary key auto_increment,
-    NAME varchar(100),
-    TEL varchar(100) unique,
+    ID         bigint primary key auto_increment,
+    NAME       varchar(100),
+    TEL        varchar(100) unique,
     AVATAR_URL varchar(1024),
-    CREATE_AT timestamp ,
-    UPDATE_AT timestamp
+    CREATE_AT  timestamp,
+    UPDATE_AT  timestamp
 
-)
+);
+INSERT INTO USER (NAME, TEL, AVATAR_URL, ADDRESS)
+VALUES ('kongmu', '13426777850', 'http://url', '火星');
+INSERT INTO USER (NAME, TEL, AVATAR_URL, ADDRESS)
+VALUES ('kongmu4', '13426777854', 'http://url', '火星');
