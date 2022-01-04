c61e4faa9ab7722351de30ce7c49f2c5c724554a
Zhouzf <zhaofengzhou9@gmail.com>
2022-01-04 22:14:48 +0800

    Fix some intagration test bugs

--- src/test/java/com/greek/shop/service/ShoppingCartIntegration.java
+++ src/test/java/com/greek/shop/service/ShoppingCartIntegration.java
@@ -1,8 +1,10 @@
 package com.greek.shop.service;
 
 import com.greek.shop.ShopApplication;
+import com.greek.shop.entity.Goods;
 import com.greek.shop.entity.Page;
 import com.greek.shop.entity.ShoppingCartData;
+import com.greek.shop.entity.ShoppingCartGoods;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.Arrays;
+
+import static java.util.stream.Collectors.toList;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -28,10 +33,27 @@ public class ShoppingCartIntegration extends AbstractIntegrationTest {
 
         Assertions.assertEquals(2, pageData.getPageNum());
         Assertions.assertEquals(1, pageData.getPageSize());
-        Assertions.assertEquals(3, pageData.getTotalPage());
+        Assertions.assertEquals(2, pageData.getTotalPage());
         Assertions.assertEquals(1, pageData.getData().size());
         Assertions.assertEquals(2, pageData.getData().get(0).getShop().getId());
 
+        Assertions.assertEquals(Arrays.asList(4L, 5L),
+                pageData.getData().get(0).getGoods()
+                        .stream()
+                        .map(Goods::getId)
+                        .collect(toList()));
+
+        Assertions.assertEquals(Arrays.asList(200, 300),
+                pageData.getData().get(0).getGoods()
+                        .stream()
+                        .map(ShoppingCartGoods::getNumber)
+                        .collect(toList()));
+
+        Assertions.assertEquals(Arrays.asList(100L, 200L),
+                pageData.getData().get(0).getGoods()
+                        .stream()
+                        .map(ShoppingCartGoods::getPrice)
+                        .collect(toList()));
 
     }
 
